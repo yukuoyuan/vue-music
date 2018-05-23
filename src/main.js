0fdb0a28e06aea0aa0726768a026d6fedef49380
于阔源 <yukuoyuan@hotmail.com>
2018-05-23 16:32:25 +0800

    引入常用的第三方插件

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
+import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
 import 'common/stylus/index.styl'
+import fastclick from 'fastclick'
 
 Vue.config.productionTip = false
-
+fastclick.attach(document.body)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
