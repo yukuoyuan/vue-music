817a5445a7fbc57fca95b9db1afc822464fdc5f2
于阔源 <yukuoyuan@hotmail.com>
2018-05-23 16:16:20 +0800

    提交了第一个版本

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    hello world
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App'
+  }
+</script>
+
+<style lang="stylus" scoped>
+  @import "~common/stylus/variable.styl"
+  #app
+    color $color-theme
+</style>
